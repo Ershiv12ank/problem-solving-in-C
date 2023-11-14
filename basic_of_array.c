763c53ad34effa4c54eca0a2d35adef01f2f82c6
SHIVANK PANDEY <128777230+Ershiv12ank@users.noreply.github.com>
2023-11-14 21:01:30 +0530

    Add files via upload

---
+++ basic_of_array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a[4]={10,20,30,40};
+printf("%d ",a[2]);
+printf("%d ",2[a]);
+printf("%d ",*(a+2));
+printf("%d ",*(2+a));
+
+
+ return 0; 
+}
---
+++ create_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int array[10]={10,203,403,293,8293,94};
+printf("%d \n",array);// return the addres of first element 
+printf("%d \n",array[0]);// value at a[1]
+printf("%d \n",array[1]);
+printf("%d \n",array[2]);
+printf("%d \n",array[3]);
+printf("%d \n",array[4]);
+printf("%d \n",array[5]);
+printf("%d \n",array[6]); // if value is not define then give the 0
+printf("%d \n",array[7]);
+ return 0; 
+}
